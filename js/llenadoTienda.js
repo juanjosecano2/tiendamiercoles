7dcc153a0603c1c6fe4e1eaff17548430f5b2e91
juan jose cano <juan.nal.743@gmail.com>
2022-03-15 20:21:36 -0500

    se actualiza el proyecto

--- js/llenadoTienda.js
+++ js/llenadoTienda.js
@@ -1,58 +1,137 @@
 // creando un arreglo de objetos
 
-    let productos =[
+let productos = [
+  {
+    foto: "img/mediacartablanco.jpg",
+    nombre: "Cuaderno media carta",
+    precio: 35000,
+    descripcion: "Cuaderno media carta color blanco",
+  },
+  {
+    foto: "img/lapiceros.png",
+    nombre: "Lapicero",
+    precio: 8000,
+    descripcion: "Lapicero personalizado",
+  },
+  {
+    foto: "img/mapasueños.png",
+    nombre: "Porta retratos",
+    precio: 8000,
+    descripcion: "Porta retratos",
+  },
+  {
+    foto: "img/mediacartaoscuro.jpg",
+    nombre: "Cuaderno media carta ",
+    precio: 35000,
+    descripcion: "Cuaderno media carta color oscuro",
+  },
+  {
+    foto: "img/mediacartablanco.jpg",
+    nombre: "Cuaderno carta",
+    precio: 55000,
+    descripcion: "Cuaderno tamaño carta color blanco",
+  },
+  {
+    foto: "img/mediacartaoscuro.jpg",
+    nombre: "Cuaderno carta",
+    precio: 55000,
+    descripcion: "Cuaderno tamaño carta color oscuro",
+  },
+  {
+    foto: "img/vasos.png",
+    nombre: "Mug",
+    precio: 8500,
+    descripcion: "Mug personalizado",
+  },
+  {
+    foto: "img/llavero.png",
+    nombre: "Llavero",
+    precio: 8000,
+    descripcion: "Llavero personalizado",
+  },
+  {
+    foto: "img/agenda.png",
+    nombre: "Agenda",
+    precio: 60000,
+    descripcion: "Agenda personalizada",
+  },
+  {
+    foto: "img/portaretratos.png",
+    nombre: "Porta retratos",
+    precio: 8000,
+    descripcion: "Porta retratos personalizado",
+  },
+];
+//como recorrer un arreglo con js
+//1.creo una variable para almacenar la base sobre la cual voy a pintar
+let fila = document.getElementById("fila");
+productos.forEach(function (producto) {
+  //pintando etiquetas
+  //div con la clase col
+  let columna = document.createElement("div");
+  columna.classList.add("col");
+  columna.classList.add("mb-5");
 
-        {foto: 'img/mediacartablanco.jpg',nombre:"cuaderno media carta",precio:35000,descripcion:"cuaderno media carta color blanco"},
-        {foto: 'img/lapiceros.png',nombre:"Lapicero",precio:8000,descripcion:"lapicero personalizado"},
-        {foto: 'img/mapasueños.png',nombre:"Mapa de sueños",precio:8000,descripcion:"lapicero personalizado"},
-        {foto: 'img/mediacartaoscuro.jpg',nombre:"cuaderno media carta ",precio:35000,descripcion:"cuaderno media carta color oscuro"},
-        {foto: 'img/mediacartablanco.jpg',nombre:"Cuaderno carta",precio:55000,descripcion:"Cuaderno tamaño carta color blanco"},
-        {foto: 'img/mediacartaoscuro.jpg',nombre:"Cuaderno carta",precio:55000,descripcion:"Cuaderno tamaño carta color oscuro"},
-        {foto: 'img/basos.png',nombre:"vasos",precio:8500,descripcion:"mug personalizado"},
-        {foto: 'img/llavero.png',nombre:"llavero",precio:8000,descripcion:"Llavero personalizado"},
-        {foto: 'img/agenda.png',nombre:"Agenda",precio:60000,descripcion:"Agenda personalizado"},
-        {foto: 'img/portaretratos.png',nombre:"Porta retratos",precio:8000,descripcion:"Porta retratos personalizado"}
+  //div con las clases card h100
 
-    ]
-    //como recorrer un arreglo con js
-    //1.creo una variable para almacenar la base sobre la cual voy a pintar
-    let fila=document.getElementById("fila")
-    productos.forEach(function(producto){
-        console.log(producto.nombre)
-        console.log(producto.precio)
-       
-        //pintando etiquetas
-        //div con la clase col
-        let columna=document.createElement("div")
-        columna.classList.add("col")
+  let tarjeta = document.createElement("div");
+  tarjeta.classList.add("card");
+  tarjeta.classList.add("h-100");
 
-        //div con las clases card h100
+  //necesito un img con la clase card-img-top
 
-        let tarjeta=document.createElement("div")
-        tarjeta.classList.add("card")
-        tarjeta.classList.add("h-100")
+  let foto = document.createElement("img");
+  foto.classList.add("card-img-top");
+  foto.src = producto.foto;
 
-        //necesito un img con la clase card-img-top
+  //cardbody
+  let cardbody = document.createElement("div");
+  cardbody.classList.add("card-body");
+  cardbody.classList.add("p-4");
 
-        let foto=document.createElement("img")
-        foto.classList.add("card-img-top")
-        foto.src=producto.foto
+  //tex
+  let titulo = document.createElement("h6");
+  titulo.classList.add("fw-bolder");
+  titulo.classList.add("text-center");
+  titulo.textContent = producto.nombre;
 
-        
+  //price
+  let precio = document.createElement("p");
+  precio.classList.add("text-center");
+  precio.textContent = "$" + producto.precio;
 
+  //description
+  let description = document.createElement("p");
+  description.classList.add("text-center");
+  description.textContent = producto.descripcion;
 
-        //tex
-        let titulo=document.createElement("h4")
-        titulo.classList.add("card-title")
-        titulo.textContent=producto.nombre
+  //products actions
+  let cardfooter = document.createElement("div");
+  cardfooter.classList.add("card-footer");
+  cardfooter.classList.add("p-4");
+  cardfooter.classList.add("pt-0");
+  cardfooter.classList.add("border-top-0");
+  cardfooter.classList.add("bg-transparent");
 
-        //padres e hijos
+  let lugarbtn = document.createElement("div");
+  lugarbtn.classList.add("text-center");
 
-        tarjeta.appendChild(titulo)
-        tarjeta.appendChild(foto)
-        columna.appendChild(tarjeta)
-        fila.appendChild(columna)
-    
-        
+  let btncard = document.createElement("a");
+  btncard.classList.add("btn");
+  btncard.classList.add("btn-outline-dark");
+  btncard.classList.add("mt-auto");
+  btncard.textContent = "Add to cart";
 
-    })
+  //padres e hijos
+
+  tarjeta.appendChild(foto);
+  tarjeta.appendChild(cardbody);
+  cardbody.appendChild(titulo);
+  cardbody.appendChild(precio);
+  cardbody.appendChild(description);
+  tarjeta.appendChild(cardfooter);
+  cardfooter.appendChild(lugarbtn);
+  lugarbtn.appendChild(btncard);
+  columna.appendChild(tarjeta);
+  fila.appendChild(columna);
+});
